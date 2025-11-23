ee88c0e0eab47021a82f067b0ed40fdc666c0404
admin <tuankaka554@gmail.com>
2025-11-23 18:47:18 +0700

    .

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ require_once './commons/function.php'; // Hàm hỗ trợ
 require_once './controllers/ProductController.php';
 require_once './controllers/LoginController.php';
 require_once './controllers/QuanlytourController.php';
+require_once './controllers/HdvController.php';
 
 // Require toàn bộ file Models
 require_once './models/ProductModel.php';
 require_once './models/LoginModel.php';
 require_once './models/QuanlytourModel.php';
+require_once './models/HdvModel.php';
 // Route
 
 if (!isset($_SESSION['user'])) {
@@ -23,6 +25,8 @@ if (!isset($_SESSION['user'])) {
 
 if(isset($_GET['mode']) ?? $_GET['mode'] =='admin') {
      require_once './routes/admin.php';
+}else if(isset($_GET['mode'])&& $_GET['mode'] =='hdv'){
+    require_once './routes/hdv.php';
 }
 else{
     require_once './routes/hdv.php';
